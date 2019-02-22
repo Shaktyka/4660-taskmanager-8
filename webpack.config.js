64a27cfd859f565d1403c2c0946ef3733a6d738b
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-02-22 19:54:26 +0500

    Мелкие правки

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require(`path`);
 module.exports = {
   mode: `development`, // режим сборки
   entry: `./src/main.js`, // точка входа
-  output: { // 
-    path: path.join(__dirname, `public`),
-    filename: `bundle.js`
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
   },
   devtool: `source-map`, // подключение sourcemap
   devServer: {
