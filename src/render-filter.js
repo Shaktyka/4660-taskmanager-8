c1bfa0c42aa85a28a6ce0852c651f8e3df489209
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-03-10 20:20:59 +0500

    Убраны лишние отступы перед параметрами тега

--- src/render-filter.js
+++ src/render-filter.js
@@ -2,7 +2,7 @@ import renderElement from './utils.js';
 
 // Рендеринг одного пункта фильтра
 const renderFilter = (filterName, taskAmount, isChecked, renderTaskList, taskContainer) => {
-  const string = `<input type="radio" id="filter__${filterName}" class="filter__input visually-hidden" name="filter" ${isChecked && taskAmount ? ` checked` : ``} ${taskAmount ? `` : ` disabled`}><label for="filter__${filterName}" class="filter__label">${filterName}<span class="filter__${filterName}-count"> ${taskAmount}</span></label>`;
+  const string = `<input type="radio" id="filter__${filterName}" class="filter__input visually-hidden" name="filter" ${isChecked && taskAmount ? `checked` : ``} ${taskAmount ? `` : `disabled`}><label for="filter__${filterName}" class="filter__label">${filterName}<span class="filter__${filterName}-count"> ${taskAmount}</span></label>`;
 
   const element = renderElement(string);
   const input = element.querySelector(`input`);
